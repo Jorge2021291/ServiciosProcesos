c8ba817925a13d6feb99afd9273aed8ff9717201
JorgeYu <jorgeyu566@gmail.com>
2023-02-17 13:59:01 +0100

    FUncionamr

--- redes/PracticaUD3/src/ACT1/Cliente.java
+++ redes/PracticaUD3/src/ACT1/Cliente.java
@@ -11,65 +11,58 @@ public class Cliente {
     public static void main(String[] arg) throws IOException {
         String Host = "localhost";
         int Puerto = 12345;
-        String NumBase;
-
-
+        String NumBase = null;
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("PROGRAMA CLIENTE INICIADO....");
         Socket Cliente = new Socket(Host, Puerto);
 
-
         System.out.println("#################### Calculadora Naútica #################### + " +
                 "'Sintaxis: operando operador operando. Operadores: + - * /. Ejemplo: 5 + 5' ");
-        System.out.println("¿Qué necesita calcular?: ");
-        NumBase = sc.nextLine();
-        System.out.println(NumBase);
 
-        while (true) {
-            String[] NumFin = NumBase.split(" ");
-            if (NumFin.length != 3) {
-                System.out.println(NumFin.length);
-                System.out.println("Tiene que tener este formato [1 + 4]");
-                System.out.println("Introduce la operacion bien: ");
-                NumBase = sc.nextLine();
+        while (NumBase != "close") {
+            System.out.println("¿Qué necesita calcular?: ");
+            NumBase = sc.nextLine();
+            System.out.println(NumBase);
 
-
-            } else {
-                String regexOperand = "-?\\d+(\\.\\d+)?"; // allows integer or decimal operands
-                String regexOperator = "[+\\-*/]";
-
-                if (!NumFin[0].matches(regexOperand) || !NumFin[1].matches(regexOperator) || !NumFin[2].matches(regexOperand)) {
-                    System.out.println("Tiene que tener este formatooo [1 + 5]");
+            while (true) {
+                String[] NumFin = NumBase.split(" ");
+                if (NumFin.length != 3) {
+                    System.out.println(NumFin.length);
+                    System.out.println("Tiene que tener este formato [1 + 4]");
                     System.out.println("Introduce la operacion bien: ");
                     NumBase = sc.nextLine();
-
                 } else {
-                    DataOutputStream flujoSalida = new
-                            DataOutputStream(Cliente.getOutputStream());
+                    String regexOperand = "-?\\d+(\\.\\d+)?"; // allows integer or decimal operands
+                    String regexOperator = "[+\\-*/]";
 
-                    flujoSalida.writeUTF(NumBase);
-                    System.out.println("Se ha enviado " + NumBase);
+                    if (!NumFin[0].matches(regexOperand) || !NumFin[1].matches(regexOperator) || !NumFin[2].matches(regexOperand)) {
+                        System.out.println("Tiene que tener este formatooo [1 + 5]");
+                        System.out.println("Introduce la operacion bien: ");
+                        NumBase = sc.nextLine();
+                    } else {
+                        DataOutputStream flujoSalida = new DataOutputStream(Cliente.getOutputStream());
+                        flujoSalida.writeUTF(NumBase);
+                        System.out.println("Se ha enviado " + NumBase);
 
-                    DataInputStream flujoEntrada = new
-                            DataInputStream(Cliente.getInputStream());
+                        DataInputStream flujoEntrada = new DataInputStream(Cliente.getInputStream());
 
-                    // EL SERVIDOR ME ENVIA UN MENSAJE
-                    System.out.println("Recibiendo del SERVIDOR: \n\t" +
-                            flujoEntrada.readUTF());
+                        // EL SERVIDOR ME ENVIA UN MENSAJE
+                        System.out.println("Recibiendo del SERVIDOR: \n\t" +
+                                flujoEntrada.readUTF());
 
-                    // CERRAR STREAMS Y SOCKETS
-                    flujoSalida.close();
-                    flujoEntrada.close();
-                    Cliente.close();
-
-
+                        // CERRAR STREAMS
+                        flujoSalida.close();
+                        flujoEntrada.close();
+                        break;
+                    }
                 }
-
             }
         }
 
+        // CERRAR SOCKET
+        Cliente.close();
     }
 }
 
--- redes/PracticaUD3/src/ACT1/Servidor.java
+++ redes/PracticaUD3/src/ACT1/Servidor.java
@@ -17,67 +17,71 @@ public class Servidor {
         System.out.println("PROGRAMA SERVIDOR INICIADO....");
 
 
-        InputStream entrada;
-        DataInputStream flujoEntrada;
-        while (true) {
+        while(true){
+            InputStream entrada;
+            DataInputStream flujoEntrada;
+            while (true) {
 
-            Cliente = serverSocket.accept();
-            System.out.println("Aceptado");
+                Cliente = serverSocket.accept();
+                System.out.println("Aceptado");
 
-            // CREO FLUJO DE ENTRADA DEL CLIENTE
+                // CREO FLUJO DE ENTRADA DEL CLIENTE
 
-            entrada = null;
-            entrada = Cliente.getInputStream();
-            flujoEntrada = new DataInputStream(entrada);
+                entrada = null;
+                entrada = Cliente.getInputStream();
+                flujoEntrada = new DataInputStream(entrada);
 
-            String NumClien = flujoEntrada.readUTF();
+                String NumClien = flujoEntrada.readUTF();
 
-            // EL CLIENTE ME ENVIA UN MENSAJE
-            System.out.println("Recibiendo del CLIENTE: \n\t" +
-                    NumClien);
-
-
-            OutputStream salida = null;
-            salida = Cliente.getOutputStream();
-            DataOutputStream flujoSalida = new DataOutputStream(salida);
+                // EL CLIENTE ME ENVIA UN MENSAJE
+                System.out.println("Recibiendo del CLIENTE: \n\t" +
+                        NumClien);
 
 
-            String[] tokens = NumClien.split(" ");
-
-            double operand1 = Double.parseDouble(tokens[0]);
-            double operand2 = Double.parseDouble(tokens[2]);
-            double result;
-            switch (tokens[1]) {
-                case "+":
-                    result = operand1 + operand2;
-                    break;
-                case "-":
-                    result = operand1 - operand2;
-                    break;
-                case "*":
-                    result = operand1 * operand2;
-                    break;
-                case "/":
-                    result = operand1 / operand2;
-                    break;
-                default:
-                    out.println("Invalid operator.");
-                    continue;
-            }
-
-            salida = Cliente.getOutputStream();
-            flujoSalida = new DataOutputStream(salida);
-
-            // ENVIO UN SALUDO AL CLIENTE
-            flujoSalida.writeUTF(String.valueOf(result));
-            out.println(result);
+                OutputStream salida = null;
+                salida = Cliente.getOutputStream();
+                DataOutputStream flujoSalida = new DataOutputStream(salida);
 
 
-            entrada.close();
-            flujoEntrada.close();
-            salida.close();
-            flujoSalida.close();
-            Cliente.close();
+                String[] tokens = NumClien.split(" ");
+
+                double operand1 = Double.parseDouble(tokens[0]);
+                double operand2 = Double.parseDouble(tokens[2]);
+                double result;
+                switch (tokens[1]) {
+                    case "+":
+                        result = operand1 + operand2;
+                        break;
+                    case "-":
+                        result = operand1 - operand2;
+                        break;
+                    case "*":
+                        result = operand1 * operand2;
+                        break;
+                    case "/":
+                        result = operand1 / operand2;
+                        break;
+                    default:
+                        out.println("Invalid operator.");
+                        continue;
+                }
+
+                salida = Cliente.getOutputStream();
+                flujoSalida = new DataOutputStream(salida);
+
+                // ENVIO UN SALUDO AL CLIENTE
+                flujoSalida.writeUTF(String.valueOf(result));
+                System.out.println(String.valueOf(result));
+
+
+                entrada.close();
+                flujoEntrada.close();
+                salida.close();
+                flujoSalida.close();
+                Cliente.close();
+        }
+
+
         }
 
 
